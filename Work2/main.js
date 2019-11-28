37a6dd4bbcd2a4069be5b3750a873f87efcf6463
Zuza Kirill <kiril733@yandex.ru>
2019-11-28 17:47:14 +0300

    Create main.js

---
+++ Work2/main.js
@@ -0,0 +1,20 @@
+function Fnc(nums, k) {
+
+  for(let i = 0; i < nums.length; i++) {
+
+    for(let j = i + 1; j < nums.length; j++) {
+
+      if ( nums[i] + nums[j] === k ) {
+
+        return true;
+
+      }
+
+    }
+
+  }
+  return false;
+}
+let nums = [10,15,3,7];
+let k = 25;
+console.log(Fnc(nums,k));
