bb3ef99bb8adacc9e9d75b25adf757b66fd46e76
Zuza Kirill <kiril733@yandex.ru>
2019-11-28 17:50:38 +0300

    Update main.js

--- Work3/main.js
+++ Work3/main.js
@@ -1,9 +1,14 @@
-function merge(nums1, m, nums2, n) 
-{
-  nums1.slice(0, m).concat(nums2).sort((a, b) => a - b).forEach((n, i) => nums1[i] = n);
-  
+/**  
+ * @param {number[]} nums1 - первый отсортированный массив  
+ * @param {number} m - количество значимых элементов в nums1  
+ * @param {number[]} nums2 - второй отсортированный массив  
+ * @param {number} n - количество элементов в nums2  
+ * @return {void} Не возвращайте ничего, вместо этого модифицируйте nums1.  
+ */  
+module.exports = function merge(nums1, m, nums2, n) {  
+   // ваш код здесь  
+  nums1.slice(0, m).concat(nums2).sort((a, b) => a - b).forEach((n, i) => nums1[i] = n);  
 }
-
 let num1 = [46,55,88,0,0,0,0];
 let num2 = [18,29,80,90];
 let k1 = 3, k2 = 4;
